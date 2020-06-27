6627074f71597d81c3db9b562665622f2de32650
LadySerena <serena.tiede+github@gmail.com>
2020-06-27 14:11:10 -0500

    feat: create separate backup directory
    this is so that when backing up the world folder the scripts don't write
    to the same place it's reading from.

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-systemctl enable minecraft.service
 mkdir -p /minecraft-data
 chown minecraft:minecraft /minecraft-data
+mkdir -p /minecraft-backup
+chown minecraft:minecraft /minecraft-backup
 chmod 0751 /minecraft-data
+chmod 0751 /minecraft-backup
+systemctl enable minecraft.service
+systemctl enable minecraft-backup.timer
+systemctl start minecraft-backup.timer
