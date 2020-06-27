fa935bb57577ee47899e5f3bfa389b85b9177727
LadySerena <serena.tiede+github@gmail.com>
2020-06-27 14:04:11 -0500

    fix: time based backups
    I created a new directory for backups to live /minecraft-backup the
    messages about tar reading a file that was written recently was because
    the backup tarball was being written to the same directory.

--- opt/minecraft/backup.sh
+++ opt/minecraft/backup.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-  echo "please specify the world name to backup"
-  exit 1
-fi
-minecraftPath="/minecraft-data"
-rconConfigPath="$minecraftPath/rcon-config.yaml"
-worldName="$1"
-time=$(date +%s)
-humanDate=$(date -R)
-echo "$time backing up world $worldName"
-rcon-cli --config $rconConfigPath say server is being backed up
-rcon-cli --config $rconConfigPath save-off
-rcon-cli --config $rconConfigPath save-all
-tar -czvf "$minecraftPath/world-$time.tar.gz" "$minecraftPath"
-gsutil cp "$minecraftPath/world-$time.tar.gz" gs://tiede-minecraft-world-bucket/
-rcon-cli --config $rconConfigPath save-on
-rcon-cli --config $rconConfigPath "say server data backed up at $humanDate"
+systemctl restart minecraft
--- opt/minecraft/shutdown.sh
+++ opt/minecraft/shutdown.sh
@@ -6,14 +6,10 @@ time=$(date +%s)
 humanDate=$(date -R)
 echo "$humanDate shutting down minecraft server"
 echo "$time backing up world $worldName"
-rcon-cli --config $rconConfigPath say server is being shutdown
-rcon-cli --config $rconConfigPath save-off
-sleep 5
-rcon-cli --config $rconConfigPath save-all
+rcon-cli --config $rconConfigPath say server is being shutdown for backups please reconnect in 10 minutes
 sleep 10
-tar -czvf "$minecraftPath/world-$time.tar.gz" "$minecraftPath/"
-gsutil cp "$minecraftPath/world-$time.tar.gz" gs://tiede-minecraft-world-bucket/
-echo "uploaded world backup"
-rcon-cli --config $rconConfigPath save-on
 rcon-cli --config $rconConfigPath stop
-sleep 10
+sleep 90
+tar -czvf "/minecraft-backup/world-$time.tar.gz" "$minecraftPath/"
+gsutil cp "/minecraft-backup/world-$time.tar.gz" gs://tiede-minecraft-world-bucket/
+echo "uploaded world backup"
