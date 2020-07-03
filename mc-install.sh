a30df7eb4bcab4781e5fd9dee9e87fd66d7018e1
LadySerena <serena.tiede+github@gmail.com>
2020-07-02 21:29:32 -0500

    feat: bump paper version to 1.16.1
    This package will be have all of the nether update features.

--- mc-install.sh
+++ mc-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get update -y
-sudo apt install /repo/build/distributions/paper-mc_1.15.2-1_amd64.deb -y
+sudo apt install /repo/build/distributions/paper-mc_1.16.1-1_amd64.deb -y
 sudo systemctl start minecraft
