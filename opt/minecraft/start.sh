92fd107741574a7afd92309910c46dd61bdba5d2
LadySerena <serena.tiede+github@gmail.com>
2020-07-27 21:48:27 -0500

    fix: use commandline flag for world dir
    due to a regression in spigot server [spigot jira
    issue](https://hub.spigotmc.org/jira/browse/SPIGOT-5824) the config file
    does not work for specifying the world container directory meaning that
    everything is saved into the current working directory of the minecraft
    jars. This meant that all of the backups are useless.

--- opt/minecraft/start.sh
+++ opt/minecraft/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/bin/java -Xmx2048M -Xms2048M -jar /opt/minecraft/paper.jar nogui
+/usr/bin/java -Xmx2048M -Xms2048M -jar /opt/minecraft/paper.jar nogui --world-container /minecraft-data
