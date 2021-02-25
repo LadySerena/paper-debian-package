717dbd5ab14d85103601d170efc073d246af24d7
LadySerena <serena.tiede+github@gmail.com>
2021-02-24 18:55:45 -0600

    feat: update paper jar
    also remove prestart script and move that functionality into the cloud
    init script for the virtual machine.

--- opt/minecraft/set-configs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#gcloud get secrets ....
-
-rcon_password=$(gcloud secrets versions access "latest" --secret="rcon-password")
-
-sed -e "s/{{rcon_password}}/$rcon_password/g" /opt/minecraft/rcon-config.yaml.template >/opt/minecraft/rcon-config.yaml
-sed -e "s/{{rcon_password}}/$rcon_password/g" /opt/minecraft/server.properties.template >/opt/minecraft/server.properties
